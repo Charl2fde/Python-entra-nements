4d57db0ff460eb7382919a5b19871617e3c00da7
Chido Le Riz <99027192+Charl2fde@users.noreply.github.com>
2023-10-24 17:44:32 +0200

    Calculatrice

---
+++ Calculatrice.py
@@ -0,0 +1,32 @@
+
+def calculatrice():
+    print("Sélectionnez une opération:")
+    print("1. Addition")
+    print("2. Soustraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choix = input("Entrez le numéro de l'opération (1/2/3/4) : ")
+
+    num1 = float(input("Entrez le premier nombre : "))
+    num2 = float(input("Entrez le deuxième nombre : "))
+
+    if choix == '1':
+        resultat = addition(num1, num2)
+        print(f"Résultat : {resultat}")
+    elif choix == '2':
+        resultat = num1 - num2
+        print(f"Résultat : {resultat}")
+    elif choix == '3':
+        resultat = num1 * num2
+        print(f"Résultat : {resultat}")
+    elif choix == '4':
+        if num2 != 0:
+            resultat = num1 / num2
+            print(f"Résultat : {resultat}")
+        else:
+            print("Erreur : Division par zéro.")
+    else:
+        print("Opération non valide.")
+
+calculatrice()
