4793a6e39e8f3da320f90208a0a72118a3fdef93
Chido Le Riz <99027192+Charl2fde@users.noreply.github.com>
2023-10-24 15:51:15 +0200

    first commit

---
+++ exemple.py
@@ -0,0 +1,39 @@
+# EXEMPLE 0
+
+# nom = "de feligonde"
+# age = "20"
+# taille = 1.83
+
+# print(nom)
+# print(age)
+# print(taille)
+
+
+
+# EXEMPLE 1
+
+# fruits = ["pomme", "banane", "orane"]
+# print(fruits(0))
+
+
+# EXEMPLE 2
+
+# def saluer(nom):
+#     print("bonjour," + nom)
+
+# saluer("Alice")
+
+
+# EXEMPLE 3
+
+# personne = {"nom":"de Feligonde", "age":20, "ville":"Paris"}
+# print(personne["nom"])
+# print(personne["ville"])
+
+# on ne peut demander qu'un champ a l afois
+
+
+# EXEMPLE 4
+
+nom = input("Entrez votre nom : ")
+print("Bonjour, " + nom)
---
+++ exo2.py
@@ -0,0 +1,15 @@
+nom = input("Entrez votre nom : ")
+age = input("Entrez votre âge : ")
+taille = input("Entrez votre taille (cm): ")
+
+print("Bonjour monsieur " + nom.upper() +", vous mesurez " + taille + "cm et vous avez " + age +" ans" )
+
+fruits = input("Entrez une liste de fruits préférés : ") 
+
+
+print("Informations personnelles :\n")
+print("Nom :", nom.upper())
+print("Âge :", age)
+print("Taille :", taille, "cm")
+print("Fruits préférés :", fruits)
+
